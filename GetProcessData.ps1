be75ffe74dac1e80182d10fcc1b62f5303238193
DominikAmann <dominik.amann@outlook.de>
2020-01-13 18:41:33 +0100

    Update GetProcessData.ps1
    Add get memory space functionalities

--- GetProcessData.ps1
+++ GetProcessData.ps1
@@ -3,21 +3,37 @@
 #########  FUNCTIONS  #########
 ###############################
 
-#Returns the working set of given process, rounded
-function Get-Working-Set-Rounded  
+#Returns the private memory of given process in mega bytes
+function Get-PrivateMemorySize-In-MegaByte
 {
     Param ([int] $processID)
+    $valueobject = Get-Process -Id $processID | Select-Object PrivateMemorySize  
+    $value = $valueobject.PrivateMemorySize  / 1mb
+    return [Math]::Round($value ,2)
+}
 
-    $workingSet = Get-Process -Id $processID | Select-Object ws 
-    $workingSetMegaByte = $workingSet.WS / 1mb
-    return [Math]::Round($workingSetMegaByte,2)
+#Returns the working set of given process in mega bytes
+function Get-WorkingSet-In-MegaByte
+{
+    Param ([int] $processID)
+    $valueobject = Get-Process -Id $processID | Select-Object ws  
+    $value = $valueobject.ws  / 1mb
+    return [Math]::Round($value ,2)
+}
+
+#Returns the virtual memory  of given process in mega bytes
+function Get-VirtualMemory-In-MegaByte
+{
+    Param ([int] $processID)
+    $valueobject = Get-Process -Id $processID | Select-Object vm  
+    $value = $valueobject.vm  /1mb
+    return [Math]::Round($value ,2)
 }
 
 ###############################
 #########  OUTPUT  ############
 ###############################
 
-#Print out working set of Process (PID) 31928
-Write-Host "Total working set of process 31928 is: $(Get-Working-Set-Rounded 31928) MB"
-
-
+Write-Host "Total private memory of process 1532 is: $(Get-PrivateMemorySize-In-MegaByte 1532) MB"
+Write-Host "Total working set of process 1532 is: $(Get-WorkingSet-In-MegaByte 1532) MB"
+Write-Host "Total virtual memory of process 1532 is: $(Get-VirtualMemory-In-MegaByte 1532) MB"
